0f2f0601ef9208baa655aad8eb1a323e4e9ccc82
Jaf4r <jaf4rdev@gmail.com>
2022-04-21 16:29:53 -0400

    Environment variables setup for login

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,32 @@
 from flask import Flask
+from flask_login import LoginManager
 # Define the WSGI application object
 app = Flask(__name__)
 
+# Configurations
+app.config.from_object('config')
+
+config = app.config
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Sample HTTP error handling
+@app.errorhandler(404)
+def not_found(error):
+    return {"error":"Endpoint not found"}, 404
+
+#If the user is not authorizewd to use the endpoint
+@login_manager.unauthorized_handler
+def unauthorized():
+    # do stuff
+    return {
+        "status":0,
+        "message":"User not authorized"
+    }, 202
+
+@login_manager.user_loader
+def load_user(user_id):
+    from .models import User
+    return User.query.get(user_id)
---
+++ config.py
@@ -0,0 +1,5 @@
+from dotenv import load_dotenv
+import os
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
